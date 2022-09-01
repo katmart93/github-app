5ca8184787e12671748118592ac2dd2bdaab0b20
katmart93 <katwlodarczyk93@gmail.com>
2022-09-01 12:23:19 +0200

    user details displayed on the page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,27 @@
+// context
+import { useContext } from "react";
+import { GithubContext } from "../context/GithubContext";
 // components
 import Container from "./Container";
 import Nav from "./Nav";
 import User from "./User";
 
 export default function Hero() {
+  const { user } = useContext(GithubContext);
+
   return (
     <section className="hero">
       <Nav />
-      <Container></Container>
+      <Container>
+        {user ? (
+          <>
+            <User />
+            <div className="wrapper"></div>
+          </>
+        ) : (
+          <p>No user found...</p>
+        )}
+      </Container>
     </section>
   );
 }
