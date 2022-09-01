633f1ea5da6ddfb9fb3c68afae8877c6e216d26e
katmart93 <katwlodarczyk93@gmail.com>
2022-09-01 11:59:38 +0200

    browser router imported

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
+import { BrowserRouter } from "react-router-dom";
 // context
 import { GithubState } from "./context/GithubContext";
-
 // components
 import Hero from "./components/Hero";
-
 // styles
 import "./App.css";
 
 function App() {
   return (
     <GithubState>
-      <Hero />
+      <BrowserRouter>
+        <Hero />
+      </BrowserRouter>
     </GithubState>
   );
 }
