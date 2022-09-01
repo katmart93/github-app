7b4d2d73ca69feb229944764047b75de1d3f42a9
katmart93 <katwlodarczyk93@gmail.com>
2022-09-01 15:18:07 +0200

    redirecting to /overview on page load

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,16 @@
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 // context
-import { useContext } from "react";
 import { GithubContext } from "../context/GithubContext";
 
 export default function Form() {
   const { search, setSearch, getSearch } = useContext(GithubContext);
+  const navigate = useNavigate();
+
+  // when the page loads, it redirects to /overview
+  useEffect(() => {
+    navigate("/overview");
+  }, []);
 
   return (
     <form onSubmit={getSearch}>
